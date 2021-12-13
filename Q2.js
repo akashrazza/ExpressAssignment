52bf028e2a4cf022842de41248f948d0e59c7009
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:15:52 +0530

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,3 +1,4 @@
+//Employee Array
 var employee_data = [
     {
         Id:1,
@@ -31,10 +32,12 @@ var app  = express();
 
 app.use(express.json());
 
+//Get All Employee GET Method
 app.get('/getAllEmployeeData',(req,res)=>{
     res.send(employee_data);
 });
 
+//Server running at 8000 port
 app.listen(8000,()=>{
     console.log("Server Started on http://localhost:8000");
 });
